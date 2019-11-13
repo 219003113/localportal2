046963e0fef03dfc2f00c6dce0596ed1128142ab
219003113 <42488276+219003113@users.noreply.github.com>
2019-11-13 12:47:36 +0530

    k

---
+++ js/salesiq.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || 
+{widgetcode:"3bf2becfdcfd805b1169c612c1ff8a3624b1dbdda5af676ad275c236a652d3de", values:{},ready:function(){}};
+var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
+s.src="https://salesiq.localzoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+</script>
